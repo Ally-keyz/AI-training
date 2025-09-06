8d889c6a58065e4d705b614d01ede404a31aafbc
Ally-keyz <manzialpe@gmail.com>
2025-09-06 08:38:31 -0600

    Learning on quick sort

--- DSA/routine/day12.py
+++ DSA/routine/day12.py
@@ -41,4 +41,29 @@ while max_val // exp > 0 :
             myArray.append(val)
     
     exp *= 10
+
+def partition(arry , low  , high) :
+    pivot = arry[high]
+    i = low- 1
     
+    for j in range(low , high):
+        if arry[j] < pivot:
+            i += 1
+        arry[i] , arry[j] = arry[j] , arry[i]
+
+    arry[i + 1] , arry[high]  = arry[high] , arry[i + 1]
+    return i + 1
+
+def quick_sort(arry , low = 0 , high = None):
+    if high is None:
+        high = len(array) - 1
+
+    if low < high:
+        pivot = partition(arry , low , high)
+        quick_sort(arry , low , pivot -1)
+        quick_sort(arry , pivot + 1 , high)
+
+
+array = [2, 6, 3, 6 , 8, 1]
+quick_sort(array)
+print("Quick sorted :" , array)     
